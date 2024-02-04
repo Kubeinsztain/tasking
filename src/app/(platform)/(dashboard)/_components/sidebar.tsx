44f4f883eb815ef0510a435971e99b9fa096040a
Jakub Radwan <kubeinsztain@gmail.com>
2024-02-04 18:38:41 +0100

    Added skeletons for loading sidebar client components

--- src/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -47,9 +47,17 @@ export const Sidebar = ({
 
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
-      <div>
-        <Skeleton />
-      </div>
+      <>
+        <div className='flex items-center justify-between mb-2 pl-2'>
+          <Skeleton className='h-5 w-[40%]' />
+          <Skeleton className='h-10 w-10' />
+        </div>
+        <div className='space-y-2'>
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
+      </>
     );
   }
 
